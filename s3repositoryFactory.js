37d9d7eaaeb8b2e4b6d8d745d7e13f385a0b4625
rong <1141591465@qq.com>
2021-01-26 13:17:28 +0800

    v1.0.2

--- s3repositoryFactory.js
+++ s3repositoryFactory.js
@@ -1,20 +1,23 @@
 const Minio = require('minio')
 const streamToPromise = require('stream-to-promise');
+const path = require('path');
+const fs = require('fs-extra');
+const R = require('ramda');
 /**
  * mys3 file repository
  */
-class  S3FileRepository {
+class S3FileRepository {
     // init config with env
-    constructor(){
-         this.minioClient = new Minio.Client({
+    constructor() {
+        this.minioClient = new Minio.Client({
             endPoint: process.env.cube_s3_endpoint,
             port: parseInt(process.env.cube_s3_port),
-            useSSL:  process.env.cube_s3_ssl=="true" ? true : false,
+            useSSL: process.env.cube_s3_ssl == "true" ? true : false,
             accessKey: process.env.cube_s3_accesskey,
             secretKey: process.env.cube_s3_secretkey
-          });
+        });
     }
-    async dataSchemaFiles() {
+    async dataSchemaFiles(includeDependencies=true) {
         var localminioClient = this.minioClient
         var bucket = process.env.cube_s3_bucket
         var Files = await streamToPromise(localminioClient.listObjectsV2(bucket, "", true))
@@ -22,14 +25,53 @@ class  S3FileRepository {
         for (const file of Files) {
             try {
                 const fileBuffer = await streamToPromise(await localminioClient.getObject(bucket, file.name))
-                let  fileItemContent = fileBuffer.toString('utf-8');
-                fileContents.push({fileName: file.name, content: fileItemContent})
+                let fileItemContent = fileBuffer.toString('utf-8');
+                fileContents.push({ fileName: file.name, content: fileItemContent })
             }
-            catch(e){
+            catch (e) {
                 console.log(e)
             }
         }
+        if (includeDependencies) {
+            fileContents = fileContents.concat(await this.readModules());
+        }
         return fileContents;
+    }
+    async readModules() {
+        const packageJson = JSON.parse(await fs.readFile('package.json', 'utf-8'));
+        const files = await Promise.all(
+            Object.keys(packageJson.dependencies).map(async module => {
+                if (R.endsWith('-schema', module)) {
+                    return this.readModuleFiles(path.join('node_modules', module));
+                }
+                return [];
+            })
+        );
+        return files.reduce((a, b) => a.concat(b));
+    }
+
+    async readModuleFiles(modulePath) {
+        const files = await fs.readdir(modulePath);
+        return (await Promise.all(
+            files.map(async file => {
+                const fileName = path.join(modulePath, file);
+                const stats = await fs.lstat(fileName);
+                if (stats.isDirectory()) {
+                    return this.readModuleFiles(fileName);
+                } else if (R.endsWith('.js', file)) {
+                    const content = await fs.readFile(fileName, 'utf-8');
+                    return [
+                        {
+                            fileName,
+                            content,
+                            readOnly: true
+                        }
+                    ];
+                } else {
+                    return [];
+                }
+            })
+        )).reduce((a, b) => a.concat(b), []);
     }
 }
 
