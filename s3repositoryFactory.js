4216994ead195406b4f64d4fff5729a497c7469a
rong fengliang <1141591465@qq.com>
2021-04-29 13:11:29 +0800

    add  try catch

--- s3repositoryFactory.js
+++ s3repositoryFactory.js
@@ -17,9 +17,9 @@ class S3FileRepository {
             useSSL: process.env.CUBEJS_S3_SSL == "true" ? true : false,
             accessKey: process.env.CUBEJS_S3_ACCESSKEY,
             secretKey: process.env.CUBEJS_s3_SECRETKEY,
-            bucket:process.env.CUBEJS_S3_BUCKET,
+            bucket: process.env.CUBEJS_S3_BUCKET,
             mod: process.env.CUBEJS_S3_MOD || "pre", // current support prod and pre
-            objectPrefix:"", // s3 object search  prefix default set ""
+            objectPrefix: "", // s3 object search  prefix default set ""
             ...restConfig,  // for better bucket with config not env
         };
         this.minioClient = new Minio.Client({
@@ -31,28 +31,32 @@ class S3FileRepository {
         });
     }
     async dataSchemaFiles(includeDependencies = true) {
+        var fileContents = []
         var self = this
         var bucket = self.config.bucket
         var objectPrefix = self.config.objectPrefix
-        let metaObjectName = self.config.mod=="prod" ? "meta.json":"meta-pre.json"
+        let metaObjectName = self.config.mod == "prod" ? "meta.json" : "meta-pre.json"
         // first fetch meta info: prod with  meta.json pre with meta-pre.json
-        let metaObjectFileBuffer = await streamToPromise(await self.minioClient.getObject(bucket, `${objectPrefix}/${metaObjectName}`))
-        let metaObjectContent = JSON5.parse(metaObjectFileBuffer.toString('utf-8'));
-        var fileContents = []
-        for (const key in metaObjectContent) {
-            if (Object.hasOwnProperty.call(metaObjectContent, key)) {
-                try {
-                    const fileBuffer = await streamToPromise(await self.minioClient.getObject(bucket, `${objectPrefix}/${key}`))
-                    let fileItemContent = fileBuffer.toString('utf-8');
-                    fileContents.push({ fileName: key, content: fileItemContent })
-                }
-                catch (e) {
-                    console.log(e)
+        try {
+            let metaObjectFileBuffer = await streamToPromise(await self.minioClient.getObject(bucket, `${objectPrefix}/${metaObjectName}`).catch(err => { console.log("some wrong", err) }))
+            let metaObjectContent = JSON5.parse(metaObjectFileBuffer.toString('utf-8'));
+            for (const key in metaObjectContent) {
+                if (Object.hasOwnProperty.call(metaObjectContent, key)) {
+                    try {
+                        const fileBuffer = await streamToPromise(await self.minioClient.getObject(bucket, `${objectPrefix}/${key}`))
+                        let fileItemContent = fileBuffer.toString('utf-8');
+                        fileContents.push({ fileName: key, content: fileItemContent })
+                    }
+                    catch (e) {
+                        console.log(e)
+                    }
                 }
             }
-        }
-        if (includeDependencies) {
-            fileContents = fileContents.concat(await this.readModules());
+            if (includeDependencies) {
+                fileContents = fileContents.concat(await this.readModules());
+            }
+        } catch (error) {
+            console.log("fetch meta info error",err)
         }
         return fileContents;
     }
